11bf57e3b172339dccd0c110fcee787db1a1a7bd
Kumari Sonia <k.sonia15896@gmail.com>
2023-03-07 00:17:57 +0530

    implemented logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import DefaultAlert from './DefaultAlert'
 
 const Navbar = (props) => {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login')
+  }
   return (
     <>
       <nav className="nav bg-dark">
         <Link className="nav-link active" aria-current="page" to="/">iNoteBook</Link>
         <Link className="nav-link" to="/">Home</Link>
         <Link className="nav-link" to="/about">About</Link>
-        {/* <div className="text-right"> */}
-        <Link className="btn btn-primary mx-2 " to="/login" role="button">Login</Link>
-        <Link className="btn btn-primary mx-2 " to="/signup" role="button">Signup</Link>
-        {/* </div> */}
+
+        {!localStorage.getItem('token') ?
+          <> <Link className="btn btn-primary mx-2 " to="/login" role="button">Login</Link>
+            <Link className="btn btn-primary mx-2 " to="/signup" role="button">Signup</Link></> :
+          <Button className="btn btn-primary mx-2 " onClick={handleLogout}>Logout</Button>
+        }
+
+
       </nav>
       {props.showAlert && <DefaultAlert message={props.message} variant={props.variant} />}
       <Outlet />
